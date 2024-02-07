83f4d72e494befb5adb04752b892fb3b74634256
Rise Erpelding <rise.erpelding@gmail.com>
2024-02-06 14:16:56 -1000

    feat: add style and markup for projects section

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+function addAriaHiddenOnResize() {
+  // if screen size is 1200px or greater, add aria-hidden to mobile gif
+  // otherwise add aria-hidden to desktop gif
+  const mobileGifs = document.getElementsByClassName('projects__gif--mobile');
+  const desktopGifs = document.getElementsByClassName('projects__gif--desktop');
+  const windowWidth = window.innerWidth;
+  
+  for (var i = 0; i < mobileGifs.length; i++) {
+    var element = mobileGifs[i];
+    if (windowWidth >= 1200) {
+      element.setAttribute('aria-hidden', 'true');
+    } else {
+      element.removeAttribute('aria-hidden');
+    }
+  }
+
+  for (var i = 0; i < desktopGifs.length; i++) {
+    var element = desktopGifs[i];
+    if (windowWidth >= 1200) {
+      element.removeAttribute('aria-hidden');
+    } else {
+      element.setAttribute('aria-hidden', 'true');
+    }
+  }
+}
+
+window.addEventListener('resize', addAriaHiddenOnResize);
+
+addAriaHiddenOnResize();
